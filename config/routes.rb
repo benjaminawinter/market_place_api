ff9e014a364131023f9c20ea9b102271d432395a
benjaminawinter <benjamin.a.winter@gmail.com>
2016-06-22 21:36:52 -0500

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MarketPlaceApi::Application.routes.draw do
 
 	# Api definition
-	namespace :apim defaults: { format: :json} do,
+	namespace :api defaults: { format: :json} do,
 					constraints: { subdomain: 'api' }, path: '/' do
 		#We are going to list our resources here	
 	end
