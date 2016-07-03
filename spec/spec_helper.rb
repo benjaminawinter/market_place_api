d19eeef7c0534bb36558b3219d78ec24880ebeee
benjaminawinter <benjamin.a.winter@gmail.com>
2016-07-03 12:08:06 -0500

    Adds show action the users controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,3 +40,11 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
